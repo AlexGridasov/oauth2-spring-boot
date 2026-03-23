4f6fae7feab76ea471fa5de8ed188efefcf32309
Alex Gridasov <AlexGridasov@users.noreply.github.com>
2026-03-23 14:40:16 +0200

    Create resource-server module

---
+++ resource-server/src/test/java/com/gri/alex/ResourceServerApplicationTest.java
@@ -0,0 +1,13 @@
+package com.gri.alex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerApplicationTest {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
