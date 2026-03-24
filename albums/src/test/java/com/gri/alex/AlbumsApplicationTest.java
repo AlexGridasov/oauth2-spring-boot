70c63e9215cbcb264802fcc558e27bf574c5c2e6
Alex Gridasov <AlexGridasov@users.noreply.github.com>
2026-03-24 18:17:57 +0200

    albums - Create albums module

---
+++ albums/src/test/java/com/gri/alex/AlbumsApplicationTest.java
@@ -0,0 +1,12 @@
+package com.gri.alex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumsApplicationTest {
+
+  @Test
+  void contextLoads() {
+  }
+}
