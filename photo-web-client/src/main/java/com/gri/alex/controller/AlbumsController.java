652db522e4757619037cb26633303c1b1f34994e
Alex Gridasov <AlexGridasov@users.noreply.github.com>
2026-04-09 17:40:58 +0300

    photo-web-client - Read JWT Access Token from OAuth2AuthorizedClient

--- photo-web-client/src/main/java/com/gri/alex/controller/AlbumsController.java
+++ photo-web-client/src/main/java/com/gri/alex/controller/AlbumsController.java
@@ -2,7 +2,12 @@ package com.gri.alex.controller;
 
 import com.gri.alex.dto.AlbumDto;
 import java.util.List;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.OidcIdToken;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
@@ -12,12 +17,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AlbumsController {
 
+  OAuth2AuthorizedClientService oauth2ClientService;
+
+  public AlbumsController(OAuth2AuthorizedClientService oauth2ClientService) {
+    this.oauth2ClientService = oauth2ClientService;
+  }
+
   @GetMapping("/albums")
-  public String getAlbums(Model model, @AuthenticationPrincipal OidcUser principal) {
-    System.out.println("Principal: " + principal);
+  public String getAlbums(Model model,
+                          @AuthenticationPrincipal OidcUser principal) {
+
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+
+    OAuth2AuthorizedClient oauthAuthorizedClient =
+        oauth2ClientService.loadAuthorizedClient(
+            oauthToken.getAuthorizedClientRegistrationId(),
+            oauthToken.getName());
+    String jwtAccessToken = oauthAuthorizedClient.getAccessToken().getTokenValue();
+    System.out.println("-> JWT Access Token: " + jwtAccessToken);
+
+    System.out.println("-> Principal: " + principal);
     OidcIdToken idToken = principal.getIdToken();
     String tokenValue = idToken.getTokenValue();
-    System.out.println("ID Token: " + tokenValue);
+    System.out.println("-> ID Token: " + tokenValue);
 
     AlbumDto album1 = new AlbumDto();
     album1.setAlbumId("1");
