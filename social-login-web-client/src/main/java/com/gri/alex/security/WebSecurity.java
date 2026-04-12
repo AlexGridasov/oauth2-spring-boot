002843067bd0b9115a7dc6a6c4440dd4e17dd012
Alex Gridasov <AlexGridasov@users.noreply.github.com>
2026-04-12 12:09:36 +0300

    photo-web-client - Implement OidcClientInitiatedLogoutSuccessHandler

--- social-login-web-client/src/main/java/com/gri/alex/security/WebSecurity.java
+++ social-login-web-client/src/main/java/com/gri/alex/security/WebSecurity.java
@@ -3,9 +3,17 @@ package com.gri.alex.security;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+  private final ClientRegistrationRepository clientRegistrationRepository;
+
+  public WebSecurity(ClientRegistrationRepository clientRegistrationRepository) {
+    this.clientRegistrationRepository = clientRegistrationRepository;
+  }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
@@ -15,9 +23,17 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         .anyRequest().authenticated()
         .and().oauth2Login()
         .and().logout()
-        .logoutSuccessUrl("/")
+        // .logoutSuccessUrl("/")
+        .logoutSuccessHandler(oidcLogoutSuccessHandler())
         .invalidateHttpSession(true)
         .clearAuthentication(true)
         .deleteCookies("JSESSIONID");
+  }
+
+  private OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler() {
+    OidcClientInitiatedLogoutSuccessHandler successHandler =
+        new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+    successHandler.setPostLogoutRedirectUri("http://localhost:8080/");
+    return successHandler;
   }
 }
